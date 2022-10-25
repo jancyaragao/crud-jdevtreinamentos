b85c31a9a9a4c0ed5fb1ed6d48f73d44aa9390ba
Jâncy Aragão <jancy.aragao@gmail.com>
2022-10-25 11:34:28 -0300

    Commit inicial

---
+++ src/main/java/com/jancyaragao/jdevtreinamentos/JdevtreinamentosApplication.java
@@ -0,0 +1,13 @@
+package com.jancyaragao.jdevtreinamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdevtreinamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdevtreinamentosApplication.class, args);
+	}
+
+}
